374e56e164369bdfa92b496bc39689da505cc041
me <svetameanssun@gmail.com>
2023-04-13 18:53:15 +0200

    trying

---
+++ ThirdTask.java
@@ -0,0 +1,20 @@
+// 3. Реализовать алгоритм пирамидальной сортировки (HeapSort).
+//        На шахматной доске расставить 8 ферзей так, чтобы они не били друг друга.
+
+
+public class ThirdTask {
+
+    public String [] Chess(){
+        for (int i = 0; i < 9; i=i) {
+            for (int j = 0; j < 9; j++) {
+                System.out.println("-");
+                if
+
+
+            }
+
+        }
+
+    }
+
+}
